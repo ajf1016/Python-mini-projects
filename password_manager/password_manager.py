56b4622b637132d768b68220d8b1521ca66c8eb4
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-02-12 01:06:26 +0530

    encrypted

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,18 +1,47 @@
-pwd = input("What is the master password? : ")
+from cryptography.fernet import Fernet
+
+
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_pwd = input("What is the master password? : ")
+key = load_key()
+fer = Fernet(key)
+
+# write_key()
+
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User name :", user, ", Pasword :",
+                  fer.decrypt(passw.encode()).decode())
+
 
 def add():
     name = input("Enter your name : ")
     pwd = input("Enter your password : ")
-    
+
     with open("passwords.txt", "a") as f:
-        f.write(name + " | " + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones? - (view,add, press Q to quite) : ").lower()
-    
+    mode = input(
+        "Would you like to add a new password or view existing ones? - (view,add, press Q to quite) : ").lower()
+
     if mode == "q":
         break
     elif mode == "view":
